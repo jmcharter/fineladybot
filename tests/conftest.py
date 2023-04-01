4a05bd47f4b96da7a543b87fb50bcb57089fcd6a
Jon <jmcharter@protonmail.com>
2023-04-01 14:00:07 +0100

    Add tests for opt_out methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,27 @@
 import pytest
+from logging import Logger
+from unittest.mock import Mock, MagicMock
+
 import praw.models  # type: ignore
+
+from fineladybot.database import Database
+
+mock_logger = Mock(spec=Logger)
 
 
 @pytest.fixture
-def subreddit() -> praw.models.Subreddit:
-    return praw.models.Subreddit(None, "testsub")
+def reddit() -> praw.Reddit:
+    reddit = MagicMock(spec=praw.Reddit)
+    reddit.subreddit = praw.models.SubredditHelper(reddit, None)
+    return reddit
+
+
+@pytest.fixture
+def subreddit(reddit) -> praw.models.Subreddit:
+    return praw.models.Subreddit(reddit, "testsub")
+
+
+@pytest.fixture(scope="session")
+def db():
+    database = Database("test_db", mock_logger, True)
+    return database
